bec37bfaedf943e525734b38131341ae831607ad
EmanuelDms <emanuel.duarte.maia@gmail.com>
2021-03-11 11:57:42 -0300

    feat: challenge 2

---
+++ challenge-02/main.js
@@ -0,0 +1,88 @@
+// # Desafio da semana #2 (REFATORAR FUTURAMENTE)
+
+// Nesse exercício, você está livre para escolher os nomes para suas variáveis e funções! (OK!)
+
+// Crie uma função que receba dois argumentos e retorne a soma dos mesmos.
+var sum = (x, y) => x + y;
+
+// Declare uma variável que receba a invocação da função criada acima, passando dois números quaisquer por argumento, e somando `5` ao resultado retornado da função.
+var foo = sum(3, 2);
+
+// Qual o valor atualizado dessa variável?
+console.log(foo);
+
+// Declare uma nova variável, sem valor.
+var myvar;
+/*
+Crie uma função que adicione um valor à variável criada acima, e retorne a string:
+    O valor da variável agora é VALOR.
+Onde VALOR é o novo valor da variável.
+*/
+function addValueToVariable() {
+    myvar = 10;
+    return 'O valor da variável agora é ' + myvar + '.';
+}
+// Invoque a função criada acima.
+console.log(addValueToVariable());
+console.log(myvar);
+// Qual o retorno da função? (Use comentários de bloco).
+/* 
+"O valor da variável agora é 10."
+ */
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos;
+2. Se qualquer um dos três argumentos não estiverem preenchidos, a função deve retornar a string:
+    Preencha todos os valores corretamente!
+3. O retorno da função deve ser a multiplicação dos 3 argumentos, somando `2` ao resultado da multiplicação.
+*/
+function calcExpression(x, y, z) {
+
+    /* Converts numbers to false and check if they are number */
+    const areNumbersValid = (x === undefined || y === undefined || z === undefined);
+
+    if (areNumbersValid) {
+        return 'Preencha todos os valores corretamente!';
+    }
+    return (x * y * z) + 2;
+}
+
+// Invoque a função criada acima, passando só dois números como argumento.
+console.log(calcExpression(1, 3));
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+// 'Preencha todos os valores corretamente!'    
+
+// Agora invoque novamente a função criada acima, mas passando todos os três argumentos necessários.
+console.log(calcExpression(1, 2, 3));
+
+// Qual o resultado da invocação acima? (Use comentários para mostrar o valor retornado).
+// 8
+
+/*
+Crie uma função com as seguintes características:
+1. A função deve receber 3 argumentos.
+2. Se somente um argumento for passado, retorne o valor do argumento.
+3. Se dois argumentos forem passados, retorne a soma dos dois argumentos.
+4. Se todos os argumentos forem passados, retorne a soma do primeiro com o segundo, e o resultado, dividido pelo terceiro.
+5. Se nenhum argumento for passado, retorne o valor booleano `false`.
+6. E ainda, se nenhuma das condições acima forem atendidas, retorne `null`.
+*/
+function calcExpression1(x, y, z) {
+    if (x && y === undefined && z === undefined)
+        return x;
+    else if (x && y && z === undefined)
+        return x + y;
+    else if (x && y && z)
+        return (x + y) / z;
+    else if (x === undefined && y === undefined && z === undefined) return false;
+    else return null;
+}
+// Invoque a função acima utilizando todas as possibilidades (com nenhum argumento, com um, com dois e com três.) Coloque um comentário de linha ao lado da função com o resultado de cada invocação.
+console.log();
+console.log(calcExpression1()); // Nenhum argumento
+console.log(calcExpression1(34)); // Um argumento
+console.log(calcExpression1(1, 5)); // Dois argumentos
+console.log(calcExpression1(1, 2, 3)); // Três argumentos
+console.log(calcExpression1('3')); // Nenhuma das opções (?)
